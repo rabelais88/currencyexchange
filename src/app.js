364a964946b2cb8638143a516147616986b61a06
rabelais88 <sungryeolp@gmail.com>
2018-02-21 13:02:12 +0900

    second commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import request from 'superagent'
+import moment from 'moment'
+
+class CurrencyExchange extends React.Component {
+  constructor(props){
+    super(props)
+  }
+  componentDidMount(){
+    
+  }
+  render(){
+
+  }
+}
+
+ReactDOM.render(<CurrencyExchange />,document.getElementById('app'))
