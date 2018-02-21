c576d64d8de84f7d5b9a8461649c430aec7ba4a3
rabelais88 <sungryeolp@gmail.com>
2018-02-21 15:55:55 +0900

    added material-ui

--- src/app.js
+++ src/app.js
@@ -2,30 +2,45 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import request from 'superagent'
 import moment from 'moment'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class CurrencyExchange extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      currencydata:{},
-      loaded:false
+      rawcurrencydata:{},
+      filtereddata:{},
+      loading:true
     }
   }
   componentDidMount(){
     request.get('/jsondata')
     .end((err,data)=>{
       if(data){
-        this.setState({currentcydata:JSON.parse(data.body),loaded:true})
+        const rawdata = JSON.parse(data.body)
+        const filtered = rawdata.filter(el=>{
+          return el.cur_unit === 'CNH' || el.cur_unit === 'USD' || el.cur_unit === 'EUR' || el.cur_unit === 'JPY(100)'
+        })
+        this.setState({rawcurrentcydata:rawdata,loading:false})
+        console.log(rawdata)
+        console.log('filtered',filtered)
       }
-      console.log(JSON.parse(data.body))
     })
   }
   render(){
-    return(
-    <div>
-      {this.state.loaded ? <span>자료가 로드되었습니다</span>: <span>아직 자료를 읽고 있습니다</span>}
-    </div>
-    )
+    if(this.state.loading){
+      return(
+        <MuiThemeProvider>
+          <div>아직 자료를 읽고 있습니다</div>
+        </MuiThemeProvider>
+      )
+    }else{
+      return(
+        <MuiThemeProvider>
+        <div>{this.state.loading ?  <span>아직 자료를 읽고 있습니다</span>: <span>자료가 로드되었습니다</span>}</div>
+        </MuiThemeProvider>
+      )
+    }
   }
 }
 
